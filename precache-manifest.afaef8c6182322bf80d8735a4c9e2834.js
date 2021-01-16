0e427c3febb99f7f90f8ea2bf99f1459a808158d
sosonian <sosonian@gmail.com>
2021-01-16 12:12:50 +0000

    Updates

---
+++ precache-manifest.afaef8c6182322bf80d8735a4c9e2834.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "358883082a4373384c97f4c80d803255",
+    "url": "/demoapp/index.html"
+  },
+  {
+    "revision": "18f4aa4ba0adea2e65dd",
+    "url": "/demoapp/static/css/main.7df83f30.chunk.css"
+  },
+  {
+    "revision": "ccf1928b920d46d207d2",
+    "url": "/demoapp/static/js/2.a2a6c5b4.chunk.js"
+  },
+  {
+    "revision": "249cadddda703681677e52e77af54cb8",
+    "url": "/demoapp/static/js/2.a2a6c5b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18f4aa4ba0adea2e65dd",
+    "url": "/demoapp/static/js/main.6e35a07d.chunk.js"
+  },
+  {
+    "revision": "a4111a88d1ddb38836b5",
+    "url": "/demoapp/static/js/runtime-main.d684157a.js"
+  },
+  {
+    "revision": "cefd34708ea82d4b72ab814a78aea52b",
+    "url": "/demoapp/static/media/TFILogo.cefd3470.svg"
+  },
+  {
+    "revision": "eb63757841f738b5a7e4db60b07d9669",
+    "url": "/demoapp/static/media/Taiwan_Film_Institute_title_20170123.eb637578.jpg"
+  }
+]);
