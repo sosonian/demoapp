d052cab839fa3f8d425a468f137859f5a6c4fa48
sosonian <sosonian@gmail.com>
2021-01-17 14:05:19 +0000

    improve code

--- src/MainBody/MainBody.js
+++ src/MainBody/MainBody.js
@@ -497,7 +497,7 @@ class MainBody extends Component {
                 </div>
                 <div className="MainContainer" >     
                     <div className="BufferArea"/>
-                    <Route exact path='/' render={props=>(<HomeBody {...props} windowSize={this.state.windowSize}/>)}/>
+                    <Route path='/' render={props=>(<HomeBody {...props} windowSize={this.state.windowSize}/>)}/>
                     <Route path='/default' render={props=>(<HomeBody {...props} windowSize={this.state.windowSize}/>)}/>
                     <Route path='/Movie/Workinfo/:movie_id' component={MovieWorkInfoMainBody}/>
                     <Route path='/Event/:Event_SysID' component={EventPage}/>
--- src/UpBanner/LogoButton.js
+++ src/UpBanner/LogoButton.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react';
-import {ReactComponent as TFILogo} from '../Utilites/TFILogo.svg'
 import './LogoButton.css'
-import TFILogoComponent from '../Utilites/TFILogo'
-import NewLogo from '../Utilites/NewLogo'
+
 
 
 class LogoButton extends Component {
